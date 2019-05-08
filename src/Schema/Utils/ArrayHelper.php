5434827a7fa21c9ff8a291e1935a5fa6ac304a46
Dmitriy Lezhnev <lezhnev.work@gmail.com>
2019-05-08 14:16:53 +0500

    add comment to ArrayHelper

--- src/Schema/Utils/ArrayHelper.php
+++ src/Schema/Utils/ArrayHelper.php
@@ -4,6 +4,17 @@ namespace OpenAPIValidation\Schema\Utils;
 
 final class ArrayHelper
 {
+    /**
+     * Check if array has non-numeric keys
+     * 
+     * JSON's objects and arrays are caster to PHP arrays.
+     * To distinguish the two it evaluates keys of PHP array:
+     * - if there are only numeric keys (0...N) then it returns true
+     * - otherwise, if there are string keys it returns false
+     *
+     * @param array $arr
+     * @return bool
+     */
     public static function isAssoc(array $arr): bool
     {
         if ([] === $arr) {
