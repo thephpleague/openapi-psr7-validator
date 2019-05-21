f501ac8ecb1b2c7f300a9eb399859829eb1f8796
Dmitriy Lezhnev <lezhnev.work@gmail.com>
2019-05-21 21:32:30 +0500

    Add comment to InvalidSchema

--- src/Schema/Exception/InvalidSchema.php
+++ src/Schema/Exception/InvalidSchema.php
@@ -10,9 +10,15 @@ use Throwable;
 // Something wrong with the OpenAPI schema. This sort of errors should have been caught by cebe's underlying package.
 final class InvalidSchema extends RuntimeException
 {
+    /**
+     * This exception can be thrown if unexpected schema values, or data values are detected
+     * for example, 'minLength' keyword expects data to be 'string', if something else given - this exception raises
+     *
+     * @return InvalidSchema
+     */
     public static function becauseDefensiveSchemaValidationFailed(Throwable $e) : self
     {
-        return new static('Schema validation failed: ' . $e->getMessage(), $e->getCode(), $e);
+        return new static('Schema(or data) validation failed: ' . $e->getMessage(), $e->getCode(), $e);
     }
 
     public static function becauseTypeIsNotKnown(string $type) : self
