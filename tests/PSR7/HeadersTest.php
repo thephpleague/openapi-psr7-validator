0cf36e5e978f833640a78ecadb55164e932f6352
Dmitriy Lezhnev <lezhnev.work@gmail.com>
2019-07-09 16:25:02 +0500

    Add test to check that header names validation is case insensitive

---
+++ tests/PSR7/HeadersTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenAPIValidationTests\PSR7;
+
+use GuzzleHttp\Psr7\ServerRequest;
+use GuzzleHttp\Psr7\Uri;
+use OpenAPIValidation\PSR7\ValidatorBuilder;
+
+final class HeadersTest extends BaseValidatorTest
+{
+    public function testItValidatesRequestQueryArgumentsGreen() : void
+    {
+        $request = (new ServerRequest('get', new Uri('/path1?queryArgA=20')))->withHeader('header-a', 'value A');
+
+        $validator = (new ValidatorBuilder())->fromYamlFile($this->apiSpecFile)->getServerRequestValidator();
+        $validator->validate($request);
+        $this->addToAssertionCount(1);
+    }
+}
