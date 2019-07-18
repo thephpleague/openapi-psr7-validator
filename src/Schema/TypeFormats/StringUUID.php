a190b5b3234f7bc45a2bc22138e03a7e1c922301
Dmitriy Lezhnev <lezhnev.work@gmail.com>
2019-07-18 19:45:06 +0500

    use case insensitive pattern matching

--- src/Schema/TypeFormats/StringUUID.php
+++ src/Schema/TypeFormats/StringUUID.php
@@ -10,8 +10,8 @@ class StringUUID
 {
     public function __invoke(string $value) : bool
     {
-        $patternUUIDV4 = '/^[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}$/';
+        $pattern = '/^[0-9a-f]{8}\-[0-9a-f]{4}\-[0-9a-f]{4}\-[0-9a-f]{4}\-[0-9a-f]{12}$/i';
 
-        return (bool) preg_match($patternUUIDV4, $value);
+        return (bool) preg_match($pattern, $value);
     }
 }
