a5bffa8521c52d0020fb97c04acc7faea87cc1e1
Krishnaprasad MG <sunspikes@gmail.com>
2019-09-20 13:58:52 +0200

    Removed duplicate reference resolves

--- src/PSR7/SchemaFactory/JsonFileFactory.php
+++ src/PSR7/SchemaFactory/JsonFileFactory.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace OpenAPIValidation\PSR7\SchemaFactory;
 
 use cebe\openapi\Reader;
-use cebe\openapi\ReferenceContext;
 use cebe\openapi\spec\OpenApi;
 use function realpath;
 
@@ -14,8 +13,6 @@ final class JsonFileFactory extends FileFactory
     public function createSchema() : OpenApi
     {
         $schema = Reader::readFromJsonFile($this->getFilename());
-
-        $schema->resolveReferences(new ReferenceContext($schema, realpath($this->getFilename())));
 
         return $schema;
     }
--- src/PSR7/SchemaFactory/YamlFileFactory.php
+++ src/PSR7/SchemaFactory/YamlFileFactory.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace OpenAPIValidation\PSR7\SchemaFactory;
 
 use cebe\openapi\Reader;
-use cebe\openapi\ReferenceContext;
 use cebe\openapi\spec\OpenApi;
 use function realpath;
 
@@ -14,8 +13,6 @@ final class YamlFileFactory extends FileFactory
     public function createSchema() : OpenApi
     {
         $schema = Reader::readFromYamlFile($this->getFilename());
-
-        $schema->resolveReferences(new ReferenceContext($schema, realpath($this->getFilename())));
 
         return $schema;
     }
