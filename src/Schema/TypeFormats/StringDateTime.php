f5523548c60995965badfdb0276b2fce137f3676
Krishnaprasad MG <sunspikes@gmail.com>
2019-10-11 13:27:32 +0200

    Remove custom datetime class

--- src/Schema/TypeFormats/StringDateTime.php
+++ src/Schema/TypeFormats/StringDateTime.php
@@ -4,12 +4,26 @@ declare(strict_types=1);
 
 namespace OpenAPIValidation\Schema\TypeFormats;
 
-use OpenAPIValidation\Foundation\Rfc3339DatetimeFactory;
+use DateTime;
 
 class StringDateTime
 {
+    /**
+     * RFC 3339, section 5.6 DateTime format
+     * for example, 2017-07-21T17:32:28Z or with optional fractional seconds, 2017-07-21T17:32:28.123Z.
+     *
+     * @var array<string>
+     */
+    private static $dateTimeFormats = ['Y-m-d\TH:i:sP', 'Y-m-d H:i:sP', 'Y-m-d\TH:i:s.uP', 'Y-m-d H:i:s.uP'];
+
     public function __invoke(string $value) : bool
     {
-        return Rfc3339DatetimeFactory::createFromString($value) !== null;
+        foreach (self::$dateTimeFormats as $dateTimeFormat) {
+            if (DateTime::createFromFormat($dateTimeFormat, $value) !== false) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
