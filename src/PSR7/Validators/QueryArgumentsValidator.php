04b0ff3e3f202d9926678971dda1805bc5961c7f
Samuel Nogueira <samuel.nogueira@jumia.com>
2019-12-19 19:06:28 +0000

    Added support for JSON query parameters

--- src/PSR7/Validators/QueryArgumentsValidator.php
+++ src/PSR7/Validators/QueryArgumentsValidator.php
@@ -16,8 +16,13 @@ use League\OpenAPIValidation\Schema\SchemaValidator;
 use Psr\Http\Message\MessageInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use const JSON_ERROR_NONE;
 use function array_key_exists;
+use function json_decode;
+use function json_last_error;
+use function json_last_error_msg;
 use function parse_str;
+use function preg_match;
 
 /**
  * @see https://swagger.io/docs/specification/describing-parameters/
@@ -75,20 +80,33 @@ final class QueryArgumentsValidator implements MessageValidator
     /**
      * @param mixed[]     $parsedQueryArguments [limit=>10]
      * @param Parameter[] $specs
+     *
+     * @throws InvalidQueryArgs
      */
     private function validateAgainstSchema(OperationAddress $addr, array $parsedQueryArguments, int $validationStrategy, array $specs) : void
     {
         // Note: By default, OpenAPI treats all request parameters as optional.
 
         foreach ($parsedQueryArguments as $name => $argumentValue) {
-            // skip if there are no schema for this argument
+            // skip if there is no spec for this argument
             if (! array_key_exists($name, $specs)) {
                 continue;
             }
 
+            $data      = $argumentValue;
+            $parameter = $specs[$name];
+            $schema    = $parameter->schema;
+            if ($schema === null) {
+                // There should be one and only one entry in 'content'.
+                foreach ($parameter->content as $contentType => $mediaType) {
+                    $schema = $mediaType->schema;
+                    $data   = self::parseArgumentValue($argumentValue, $contentType, $addr, $name);
+                }
+            }
+
             $validator = new SchemaValidator($validationStrategy);
             try {
-                $validator->validate($argumentValue, $specs[$name]->schema, new BreadCrumb($name));
+                $validator->validate($data, $schema, new BreadCrumb($name));
             } catch (SchemaMismatch $e) {
                 throw InvalidQueryArgs::becauseValueDoesNotMatchSchema($name, $argumentValue, $addr, $e);
             }
@@ -107,5 +125,22 @@ final class QueryArgumentsValidator implements MessageValidator
         }
 
         return $parsedQueryArguments;
+    }
+
+    /**
+     * @return mixed
+     *
+     * @throws InvalidQueryArgs
+     */
+    private static function parseArgumentValue(string $value, string $contentType, OperationAddress $addr, string $argumentName)
+    {
+        if (preg_match('#^application/.*json$#', $contentType)) {
+            $value = json_decode($value, true);
+            if (json_last_error() !== JSON_ERROR_NONE) {
+                throw InvalidQueryArgs::becauseValueIsNotValidJson(json_last_error_msg(), $argumentName, $addr);
+            }
+        }
+
+        return $value;
     }
 }
---
+++ tests/FromCommunity/JsonQueryParameterValidationTest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace League\OpenAPIValidation\Tests\FromCommunity;
+
+use GuzzleHttp\Psr7\ServerRequest;
+use League\OpenAPIValidation\PSR7\Exception\ValidationFailed;
+use League\OpenAPIValidation\PSR7\ValidatorBuilder;
+use PHPUnit\Framework\TestCase;
+use function http_build_query;
+
+/**
+ * @see https://github.com/thephpleague/openapi-psr7-validator/issues/2
+ */
+final class JsonQueryParameterValidationTest extends TestCase
+{
+    public function testGoodJsonQueryParameter() : void
+    {
+        $queryParams = ['filter' => '{"type":"t-shirt","color":"blue"}'];
+        $psrRequest  = (new ServerRequest(
+            'GET',
+            'http://localhost:8000/api/v1/products?' . http_build_query($queryParams)
+        ))
+            ->withQueryParams($queryParams);
+
+        $validator = (new ValidatorBuilder())
+            ->fromYamlFile(__DIR__ . '/../stubs/serialized-params.yaml')
+            ->getServerRequestValidator();
+
+        $validator->validate($psrRequest);
+
+        $this->addToAssertionCount(1);
+    }
+
+    public function testBadJsonQueryParameter() : void
+    {
+        $queryParams = ['filter' => '{"type":"t-shirt","color":false}'];
+        $psrRequest  = (new ServerRequest(
+            'GET',
+            'http://localhost:8000/api/v1/products?' . http_build_query($queryParams)
+        ))
+            ->withQueryParams($queryParams);
+
+        $validator = (new ValidatorBuilder())
+            ->fromYamlFile(__DIR__ . '/../stubs/serialized-params.yaml')
+            ->getServerRequestValidator();
+
+        $this->expectException(ValidationFailed::class);
+
+        $validator->validate($psrRequest);
+    }
+
+    public function testInvalidJsonQueryParameter() : void
+    {
+        $queryParams = ['filter' => 'type,t-shirt,color,blue'];
+        $psrRequest  = (new ServerRequest(
+            'GET',
+            'http://localhost:8000/api/v1/products?' . http_build_query($queryParams)
+        ))
+            ->withQueryParams($queryParams);
+
+        $validator = (new ValidatorBuilder())
+            ->fromYamlFile(__DIR__ . '/../stubs/serialized-params.yaml')
+            ->getServerRequestValidator();
+
+        $this->expectException(ValidationFailed::class);
+
+        $validator->validate($psrRequest);
+    }
+}
