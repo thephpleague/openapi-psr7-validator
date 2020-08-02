f17a1dd0bd74ded4ada215ac114e60a84aab1b37
ctipggroup <30641668+ctipggroup@users.noreply.github.com>
2020-08-02 18:11:27 +1000

    Add support for style=form explode=false to serialized parameter (#72)

--- src/PSR7/Validators/SerializedParameter.php
+++ src/PSR7/Validators/SerializedParameter.php
@@ -14,6 +14,8 @@ use League\OpenAPIValidation\Schema\Exception\TypeMismatch;
 use Respect\Validation\Exceptions\ExceptionInterface;
 use Respect\Validation\Validator;
 use const JSON_ERROR_NONE;
+use function explode;
+use function in_array;
 use function is_float;
 use function is_int;
 use function is_numeric;
@@ -24,18 +26,34 @@ use function json_last_error;
 use function key;
 use function preg_match;
 use function reset;
+use function strtolower;
 
 final class SerializedParameter
 {
+    private const STYLE_FORM            = 'form';
+    private const STYLE_SPACE_DELIMITED = 'spaceDelimited';
+    private const STYLE_PIPE_DELIMITED  = 'pipeDelimited';
+    private const STYLE_DELIMITER_MAP   = [
+        self::STYLE_FORM => ',',
+        self::STYLE_SPACE_DELIMITED => ' ',
+        self::STYLE_PIPE_DELIMITED => '|',
+    ];
+
     /** @var CebeSchema */
     private $schema;
     /** @var string|null */
     private $contentType;
+    /** @var string|null */
+    private $style;
+    /** @var bool|null */
+    private $explode;
 
-    public function __construct(CebeSchema $schema, ?string $contentType = null)
+    public function __construct(CebeSchema $schema, ?string $contentType = null, ?string $style = null, ?bool $explode = null)
     {
         $this->schema      = $schema;
         $this->contentType = $contentType;
+        $this->style       = $style;
+        $this->explode     = $explode;
     }
 
     public static function fromSpec(CebeParameter $parameter) : self
@@ -45,7 +63,7 @@ final class SerializedParameter
             if ($parameter->schema !== null) {
                 Validator::not(Validator::notEmpty())->assert($content);
 
-                return new self($parameter->schema);
+                return new self($parameter->schema, null, $parameter->style, $parameter->explode);
             }
 
             Validator::length(1, 1)->assert($content);
@@ -59,7 +77,7 @@ final class SerializedParameter
         $schema      = reset($content)->schema;
         $contentType = key($content);
 
-        return new self($schema, $contentType);
+        return new self($schema, $contentType, $parameter->style, $parameter->explode);
     }
 
     /**
@@ -85,19 +103,7 @@ final class SerializedParameter
             return $decodedValue;
         }
 
-        if (($this->schema->type === CebeType::BOOLEAN) && is_scalar($value) && preg_match('#^(true|false)$#i', (string) $value)) {
-            return (bool) $value;
-        }
-
-        if (($this->schema->type === CebeType::NUMBER)
-            && is_scalar($value) && is_numeric($value)) {
-            return is_int($value) ? (int) $value : (float) $value;
-        }
-
-        if (($this->schema->type === CebeType::INTEGER)
-            && is_scalar($value) && ! is_float($value) && preg_match('#^[-+]?\d+$#', (string) $value)) {
-            return (int) $value;
-        }
+        $value = $this->castToSchemaType($value, $this->schema->type);
 
         return $value;
     }
@@ -105,6 +111,54 @@ final class SerializedParameter
     private function isJsonContentType() : bool
     {
         return $this->contentType !== null && preg_match('#^application/.*json$#', $this->contentType) !== false;
+    }
+
+    /**
+     * @param mixed $value
+     *
+     * @return mixed
+     */
+    private function castToSchemaType($value, ?string $type)
+    {
+        if (($type === CebeType::BOOLEAN) && is_scalar($value) && preg_match('#^(true|false)$#i', (string) $value)) {
+            return is_string($value) ? strtolower($value) === 'true' : (bool) $value;
+        }
+
+        if (($type === CebeType::NUMBER)
+            && is_scalar($value) && is_numeric($value)) {
+            return is_int($value) ? (int) $value : (float) $value;
+        }
+
+        if (($type === CebeType::INTEGER)
+            && is_scalar($value) && ! is_float($value) && preg_match('#^[-+]?\d+$#', (string) $value)) {
+            return (int) $value;
+        }
+
+        if (($type === CebeType::ARRAY) && is_string($value)) {
+            return $this->convertToSerializationStyle($value);
+        }
+
+        return $value;
+    }
+
+    /**
+     * @param mixed $value
+     *
+     * @return mixed
+     */
+    protected function convertToSerializationStyle($value)
+    {
+        if ($this->explode === false
+            && in_array($this->style, [self::STYLE_FORM, self::STYLE_SPACE_DELIMITED, self::STYLE_PIPE_DELIMITED], true)) {
+            $value = explode(self::STYLE_DELIMITER_MAP[$this->style], $value);
+            foreach ($value as &$val) {
+                $val = $this->castToSchemaType($val, $this->schema->items->type ?? null);
+            }
+
+            return $value;
+        }
+
+        return $value;
     }
 
     public function getSchema() : CebeSchema
---
+++ tests/FromCommunity/IssueWithQueryArrayTest.php
@@ -0,0 +1,112 @@
+<?php
+
+declare(strict_types=1);
+
+namespace League\OpenAPIValidation\Tests\FromCommunity;
+
+use GuzzleHttp\Psr7\ServerRequest;
+use League\OpenAPIValidation\PSR7\ValidatorBuilder;
+use PHPUnit\Framework\TestCase;
+
+final class IssueWithQueryArrayTest extends TestCase
+{
+    public function testConvertFormIntegerArray() : void
+    {
+        $validator = (new ValidatorBuilder())->fromYaml($this->makeYaml('form', 'integer', 'int32'))->getServerRequestValidator();
+        $validator->validate($this->makeRequest('form', 'integer'));
+        $this->addToAssertionCount(1);
+    }
+
+    public function testConvertFormNumberArray() : void
+    {
+        $validator = (new ValidatorBuilder())->fromYaml($this->makeYaml('form', 'number', 'float'))->getServerRequestValidator();
+        $validator->validate($this->makeRequest('form', 'number'));
+        $this->addToAssertionCount(1);
+    }
+
+    public function testConvertFormIntegerArrayToStringArray() : void
+    {
+        $validator = (new ValidatorBuilder())->fromYaml($this->makeYaml('form', 'string', 'int32'))->getServerRequestValidator();
+        $validator->validate($this->makeRequest('form', 'integer'));
+        $this->addToAssertionCount(1);
+    }
+
+    public function testConvertFormStringArray() : void
+    {
+        $validator = (new ValidatorBuilder())->fromYaml($this->makeYaml('form', 'string', 'int32'))->getServerRequestValidator();
+        $validator->validate($this->makeRequest('form', 'string'));
+        $this->addToAssertionCount(1);
+    }
+
+    public function testConvertFormBooleanArray() : void
+    {
+        $validator = (new ValidatorBuilder())->fromYaml($this->makeYaml('form', 'boolean', 'int32'))->getServerRequestValidator();
+        $validator->validate($this->makeRequest('form', 'boolean'));
+        $this->addToAssertionCount(1);
+    }
+
+    public function testConvertFormIntegerArrayError() : void
+    {
+        $this->expectExceptionMessage('Value "id1,id2,id3" for argument "id" is invalid for Request [get /users]');
+        $validator = (new ValidatorBuilder())->fromYaml($this->makeYaml('form', 'integer', 'int32'))->getServerRequestValidator();
+        $validator->validate($this->makeRequest('form', 'string'));
+        $this->addToAssertionCount(1);
+    }
+
+    public function testConvertSpaceIntegerArray() : void
+    {
+        $validator = (new ValidatorBuilder())->fromYaml($this->makeYaml('spaceDelimited', 'integer', 'int32'))->getServerRequestValidator();
+        $validator->validate($this->makeRequest('spaceDelimited', 'integer'));
+        $this->addToAssertionCount(1);
+    }
+
+    public function testConvertPipeIntegerArray() : void
+    {
+        $validator = (new ValidatorBuilder())->fromYaml($this->makeYaml('pipeDelimited', 'integer', 'int32'))->getServerRequestValidator();
+        $validator->validate($this->makeRequest('pipeDelimited', 'integer'));
+        $this->addToAssertionCount(1);
+    }
+
+    protected function makeYaml(string $style, string $type, string $format) : string
+    {
+        return $yaml = /** @lang yaml */
+            <<<YAML
+openapi: 3.0.0
+info:
+  title: Product import API
+  version: '1.0'
+servers:
+  - url: 'http://localhost:8000/api/v1'
+paths:
+  /users:
+    get:
+      parameters:
+        - in: query
+          name: id
+          required: true
+          style: $style
+          explode: false
+          schema:
+            type: array
+            items:
+              type: $type
+              format: $format
+      responses:
+        '200':
+          description: A list of users
+YAML;
+    }
+
+    protected function makeRequest(string $style, string $type) : ServerRequest
+    {
+        $map     = [
+            'form' => ['integer' => '1,2,3', 'string' => 'id1,id2,id3', 'boolean' => 'true,false', 'number' => '1.00,2.00,3.00'],
+            'spaceDelimited' => ['integer' => '1 2 3', 'string' => 'id1 id2 id3', 'boolean' => 'true false', 'number' => '1.00 2.00 3.00'],
+            'pipeDelimited' => ['integer' => '1|2|3', 'string' => 'id1|id2|id3', 'boolean' => 'true|false', 'number' => '1.00|2.00|3.00'],
+        ];
+        $request = new ServerRequest('GET', 'http://localhost:8000/api/v1/users');
+        $request = $request->withQueryParams(['id' => $map[$style][$type]]);
+
+        return $request;
+    }
+}
