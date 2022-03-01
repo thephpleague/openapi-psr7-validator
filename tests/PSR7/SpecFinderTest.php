fd9bede92d58a34c955036d6c3f2de45a221c0f3
Chris Wolff <cwolff@aer.de>
2022-03-01 09:20:05 +0100

    Add ResponseStatusCodesWithWildcards Test for Specfinder
    Test Different Response Adresses and check if the correct
    Response Schema is Found
    
    #158

--- tests/PSR7/SpecFinderTest.php
+++ tests/PSR7/SpecFinderTest.php
@@ -6,6 +6,7 @@ namespace League\OpenAPIValidation\Tests\PSR7;
 
 use League\OpenAPIValidation\PSR7\CallbackAddress;
 use League\OpenAPIValidation\PSR7\OperationAddress;
+use League\OpenAPIValidation\PSR7\ResponseAddress;
 use League\OpenAPIValidation\PSR7\SpecFinder;
 use League\OpenAPIValidation\PSR7\ValidatorBuilder;
 use PHPUnit\Framework\TestCase;
@@ -121,5 +122,62 @@ JSON;
         $specFinder = new SpecFinder($schema);
         $pathItem   = $specFinder->findPathSpec(new OperationAddress('/api/1.0/order/123', 'get'));
         self::assertSame('The order object', $pathItem->get->responses[200]->description);
+    }
+
+    public function testResponseStatusCodesWithWildcards(): void
+    {
+        $yaml = <<<YAML
+openapi: 3.0.0
+info:
+  title: Product import API
+  version: '1.0'
+servers:
+  - url: 'http://localhost:8000/api/v1'
+paths:
+  /products.find:
+    get:
+      responses:
+        '404':
+          description: Not Found
+          content:
+            application/json:
+              schema:
+                properties:
+                  message:
+                    type: string
+        '4XX':
+          description: Client Error
+          content:
+            application/json:
+              schema:
+                properties:
+                  message:
+                    type: string
+        'default':
+          description: Unexpected Error
+          content:
+            application/json:
+              schema:
+                properties:
+                  message:
+                    type: string
+YAML;
+
+        $schema = (new ValidatorBuilder())->fromYaml($yaml)->getServerRequestValidator()->getSchema();
+        $specFinder = new SpecFinder($schema);
+        $responseSpec = $specFinder->findResponseSpec(
+            new ResponseAddress('/products.find', 'get', 404)
+        );
+        self::assertSame('Not Found', $responseSpec->description);
+
+        $responseSpec = $specFinder->findResponseSpec(
+            new ResponseAddress('/products.find', 'get', 400)
+        );
+        self::assertSame('Client Error', $responseSpec->description);
+
+        $responseSpec = $specFinder->findResponseSpec(
+            new ResponseAddress('/products.find', 'get', 500)
+        );
+        self::assertSame('Unexpected Error', $responseSpec->description);
     }
 }
