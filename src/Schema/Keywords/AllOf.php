3fab15ef610df164a2b13ac0aebadc7075f80543
Roger Vilà <rogervila@me.com>
2022-09-02 12:47:24 +0200

    Update AllOf.php

--- src/Schema/Keywords/AllOf.php
+++ src/Schema/Keywords/AllOf.php
@@ -9,8 +9,6 @@ use League\OpenAPIValidation\Schema\BreadCrumb;
 use League\OpenAPIValidation\Schema\Exception\InvalidSchema;
 use League\OpenAPIValidation\Schema\Exception\SchemaMismatch;
 use League\OpenAPIValidation\Schema\SchemaValidator;
-use Respect\Validation\Exceptions\Exception;
-use Respect\Validation\Exceptions\ExceptionInterface;
 use Respect\Validation\Validator;
 
 final class AllOf extends BaseKeyword
@@ -47,7 +45,7 @@ final class AllOf extends BaseKeyword
         try {
             Validator::arrayVal()->assert($allOf);
             Validator::each(Validator::instance(CebeSchema::class))->assert($allOf);
-        } catch (Exception | ExceptionInterface $exception) {
+        } catch (\Throwable $exception) {
             throw InvalidSchema::becauseDefensiveSchemaValidationFailed($exception);
         }
 
