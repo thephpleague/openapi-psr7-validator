3c90fde68e7aa6c9264231ea013b6e5e9387f5e0
Roger Vilà <rogervila@me.com>
2022-09-02 12:56:06 +0200

    Update MinProperties.php

--- src/Schema/Keywords/MinProperties.php
+++ src/Schema/Keywords/MinProperties.php
@@ -6,8 +6,6 @@ namespace League\OpenAPIValidation\Schema\Keywords;
 
 use League\OpenAPIValidation\Schema\Exception\InvalidSchema;
 use League\OpenAPIValidation\Schema\Exception\KeywordMismatch;
-use Respect\Validation\Exceptions\Exception;
-use Respect\Validation\Exceptions\ExceptionInterface;
 use Respect\Validation\Validator;
 
 use function count;
@@ -34,7 +32,7 @@ class MinProperties extends BaseKeyword
         try {
             Validator::arrayType()->assert($data);
             Validator::trueVal()->assert($minProperties >= 0);
-        } catch (Exception | ExceptionInterface $e) {
+        } catch (\Throwable $e) {
             throw InvalidSchema::becauseDefensiveSchemaValidationFailed($e);
         }
 
